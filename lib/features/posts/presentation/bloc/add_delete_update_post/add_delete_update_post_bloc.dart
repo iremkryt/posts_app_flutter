5db4befdf10dca25b468b75a262c7406b7d76615
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-13 16:33:33 +0300

    unnecessary

--- lib/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart
+++ lib/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart
@@ -15,38 +15,40 @@ part 'add_delete_update_post_state.dart';
 
 class AddDeleteUpdatePostBloc
     extends Bloc<AddDeleteUpdatePostEvent, AddDeleteUpdatePostState> {
+
   final AddPostUsecase addPost;
   final DeletePostUsecase deletePost;
   final UpdatePostUsecase updatePost;
+
   AddDeleteUpdatePostBloc(
       {required this.addPost,
       required this.deletePost,
       required this.updatePost})
       : super(AddDeleteUpdatePostInitial()) {
     on<AddDeleteUpdatePostEvent>((event, emit) async {
+
       if (event is AddPostEvent) {
+
         emit(LoadingAddDeleteUpdatePostState());
-
         final failureOrDoneMessage = await addPost(event.post);
-
         emit(
           _eitherDoneMessageOrErrorState(
               failureOrDoneMessage, ADD_SUCCESS_MESSAGE),
         );
+
       } else if (event is UpdatePostEvent) {
+
         emit(LoadingAddDeleteUpdatePostState());
-
         final failureOrDoneMessage = await updatePost(event.post);
-
         emit(
           _eitherDoneMessageOrErrorState(
               failureOrDoneMessage, UPDATE_SUCCESS_MESSAGE),
         );
+
+
       } else if (event is DeletePostEvent) {
         emit(LoadingAddDeleteUpdatePostState());
-
         final failureOrDoneMessage = await deletePost(event.postId);
-
         emit(
           _eitherDoneMessageOrErrorState(
               failureOrDoneMessage, DELETE_SUCCESS_MESSAGE),
