2ff14f31d429a3d9efe745e5f319bac47387228d
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-14 14:28:44 +0300

    Created post_list_widget

---
+++ lib/features/posts/presentation/widgets/post_list_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:posts_app_flutter/features/posts/domain/entities/post.dart';
+
+class PostListWidget extends StatelessWidget {
+  final List<Post> posts;
+  const PostListWidget({super.key, required this.posts});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Text(posts[index].id.toString()),
+          title: Text(posts[index].title,
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold 
+            ),
+          ),
+          subtitle: Text(posts[index].body,
+            style: TextStyle(fontSize: 16),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          onTap: () {},
+        );
+      },
+      separatorBuilder: (context, index) => Divider(thickness: 1,),
+    );
+  }
+}
