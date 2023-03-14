d9069112cca987b739f2d928ad87fdaa1c1f6c48
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-14 14:29:24 +0300

    Created loading_widget

---
+++ lib/core/widgets/loading_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:posts_app_flutter/core/app_theme.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Center(
+        child: SizedBox(
+          height: 30,
+          width: 30,
+          child: CircularProgressIndicator(
+            color: secondaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
