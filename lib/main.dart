d92edb2877d8da4f8fac2329dd37892e69082c4d
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-14 10:43:48 +0300

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:posts_app_flutter/core/app_theme.dart';
-import 'package:posts_app_flutter/core/network/network_info.dart';
-import 'package:posts_app_flutter/features/posts/data/datasources/post_local_data_source.dart';
-import 'package:posts_app_flutter/features/posts/data/datasources/post_remote_data_source.dart';
-import 'package:posts_app_flutter/features/posts/data/repositories/post_repository_impl.dart';
-import 'package:posts_app_flutter/features/posts/domain/usecases/get_all_posts.dart';
 import 'package:posts_app_flutter/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
 import 'package:posts_app_flutter/features/posts/presentation/bloc/posts/posts_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'injection_container.dart' as di;
 
 void main() async {
-  final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  runApp(MyApp(sharedPreferences: sharedPreferences,));
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
 } 
 
 class MyApp extends StatelessWidget {
-  final SharedPreferences sharedPreferences;
-
-  const MyApp({Key? key, required this.sharedPreferences}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_)=> PostsBloc(getAllPosts: GetAllPostsUsecase(
-          PostsRepositoryImpl(
-            remoteDataSource: PostRemoteDateSourceImpl(client: Client()), 
-            localDataSource: PostLocalDateSourceImpl(sharedPreferences: sharedPreferences), 
-            networkInfo: NetworkInfoImpl(InternetConnectionChecker()),
-          )
-        )))
-        AddDeleteUpdatePostBloc(
-          addPost: , 
-          deletePost: , 
-          updatePost: ,
+        BlocProvider(
+          create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent()),
         ),
-      ], 
+        BlocProvider(
+          create: (_) => di.sl<AddDeleteUpdatePostBloc>(),
+        ),
+      ],
       child: MaterialApp(
-        theme: appTheme,
-        title: 'Posts App',
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('POSTS'),
-          ),
-          body: Center(
-            child: Container(
-              child: Text('Post Appe hoşgeldiniz'),
-            ),
-          ),
-        ),
-      ), 
+        theme: appTheme,
+        title: 'POSTS',
+        //home: PostsPage(),
+      ),
     );
 
     
