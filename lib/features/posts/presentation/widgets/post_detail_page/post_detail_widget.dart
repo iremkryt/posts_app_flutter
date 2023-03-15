6d7f60294688baf5c4c9673454cf776af669cb36
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-15 10:59:00 +0300

    Created post_detail_widget.dart

---
+++ lib/features/posts/presentation/widgets/post_detail_page/post_detail_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:posts_app_flutter/features/posts/domain/entities/post.dart';
+
+class PostDetailWidget extends StatelessWidget {
+  final Post post;
+  const PostDetailWidget({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
