8317b8d6885c0da7ce091ee2463278a8eed272f6
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-15 10:58:33 +0300

    Created post_detail_page.dart

---
+++ lib/features/posts/presentation/pages/post_detail_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:posts_app_flutter/features/posts/domain/entities/post.dart';
+import 'package:posts_app_flutter/features/posts/presentation/widgets/post_detail_page/post_detail_widget.dart';
+
+class PostDetailPage extends StatelessWidget {
+  final Post post;
+  const PostDetailPage({Key? key, required this.post}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(),
+      body: _buildBody(),
+    );
+  }
+  AppBar _buildAppbar() {
+    return AppBar(
+      title: Text("Post Detail"),
+    );
+  }
+  Widget _buildBody(){
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: PostDetailWidget(post: post),
+      ),
+    );
+  }
+}
