1fcc9dbb9b7cb3967aa53ff96e4a09508a052dc9
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-16 10:12:17 +0300

    Created post_detail_widget.dart

--- lib/features/posts/presentation/widgets/post_detail_page/post_detail_widget.dart
+++ lib/features/posts/presentation/widgets/post_detail_page/post_detail_widget.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:posts_app_flutter/features/posts/domain/entities/post.dart';
+import 'package:posts_app_flutter/features/posts/presentation/widgets/post_detail_page/delete_post_btn.dart';
+import 'package:posts_app_flutter/features/posts/presentation/widgets/post_detail_page/update_post_btn.dart';
 
 class PostDetailWidget extends StatelessWidget {
   final Post post;
@@ -8,6 +12,38 @@ class PostDetailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Text(
+            post.title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Divider(
+            height: 50,
+          ),
+          Text(
+            post.body,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Divider(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              UpdatePostBtnWidget(post: post),
+              DeletePostBtnWidget(postId: post.id!)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
